e45c7da52f807fec1c001094a7aa88921a488c95
R-koma <ryomakomazawa@gmail.com>
2025-03-28 09:49:29 +0900

    fix: friend_request handling to work normally

--- backend/migrations/migration.go
+++ backend/migrations/migration.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"github.com/R-koma/translation-app/backend/infra"
+	"github.com/R-koma/translation-app/backend/db"
 	"github.com/R-koma/translation-app/backend/models"
 )
 
 func main() {
-	infra.Initialize()
-	db := infra.SetupDB()
+	db := db.SetupDB()
 
 	if err := db.AutoMigrate(&models.User{}); err != nil {
 		panic("Failed to migrate User database")
